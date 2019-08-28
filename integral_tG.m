89613d53fac4afbf1541dbbb9296fff7348e9c6b
Dario <dario.perez@pucv.cl>
2019-08-28 15:35:05 -0300

    test G

--- integral_tG.m
+++ integral_tG.m
@@ -1,4 +1,4 @@
-function [itG, error_itG] = integral_tG(N, km, H, Rm, rm, s)
+function [itG, error_itG] = integral_tG(N, km, H, Rm, deltam, s)
 %
 % [ITG, ERROR_ITG] = INTEGRAL_TG(N, Q, kmD, kmd, s)
 %
@@ -14,7 +14,7 @@ function [itG, error_itG] = integral_tG(N, km, H, Rm, rm, s)
 %
 %   RM = km D /2 is the dimensionless entrance pupil
 %
-%   rm = km d is the dimensionless separation between dots in the array.
+%   deltam = km d is the dimensionless separation between dots in the array.
 %
 %   S is the layer position where IT is evaluated.
 %   48 = 32.*gamma(2H+2) para H=1/3 -> gamma(8/3)=1.5 aprox.
@@ -32,7 +32,7 @@ for k=1:n
         et = @(x)((32*gamma(2.*H+2).*km.^(1-2.*H)./Rm.^2.*sin(pi.*H))...
             .*x.^(-8/3)...
             .*(besselj(1,s(k).*Rm.*x)).^2 ...
-            .*(1-besselj(0,(1-s(k)).*rm.*x)));
+            .*(1-besselj(0,(1-s(k)).*deltam.*x)));
     
         [itG(k),error_itG(k)]= quadgk(et,0,N,'RelTol',0,'AbsTol',1e-10,... 
             'MaxIntervalCount',10468);%1e-13
